b4b476ca064d78c31a71e8da1a433836cb0d30cc
Anthony Perez <36647150+Perezanthony117@users.noreply.github.com>
2018-07-14 20:35:51 -0600

    Fixed all errors

--- GameOfLifeClean/wwwroot/js/site.js
+++ GameOfLifeClean/wwwroot/js/site.js
@@ -13,41 +13,6 @@ var startGame;
 
 function drawGrid() {
 
-    user = new user();
-
-    connection = new WebSocketManager.Connection("ws://localhost:5000/server");
-
-    connection.connectionMethods.onConnected = () => {
-        user.id = connection.connectionId;
-                          //("Name of Method to invoke on server",SocketID,Content to pass)
-        connection.invoke("ConnectedUser", connection.connectionId, JSON.stringify(user));
-
-    }
-
-    connection.connectionMethods.onDisconnected = () => {
-
-        connection.invoke("DisconnectedUser", connection.connectionId,"");
-
-    }
-
-    connection.clientMethods["pingUsers"] = (serUsers) => {
-        users = JSON.parse(serUser);
-        console.log(users);
-    };
-
-    connection.clientMethods["ReceiveUpdateAsXYColor"] = (socketId, x, y, r, g, b) => {
-        // console.log(r + " " + g + " " + b);
-        var colorToUse = "#" + r + g + b;
-        // console.log(colorToUse + "(" + x + "," + y + ")");
-        assignColorToSquare(x,y,colorToUse);
-    };
-
-    connection.start()
-
-    $(window).unload(function () {
-        connection.invoke("DisconnectedUser", connection.connectionId, "");
-    });
-
     var c = document.getElementById("gameOfLife");
     var ctx = c.getContext("2d");
     ctx.clearRect(0, 0, totalWidth, totalHeight); // clear the canvass
@@ -137,4 +102,41 @@ function Stop(){
     connection.invoke("stopGame", connection.connectionId, JSON.stringify(startGame));
 }
 
-window.onload = drawGrid();
+window.onload = function () {
+
+    connection = new WebSocketManager.Connection("ws://localhost:5000/server");
+    drawGrid();
+
+    user = new user();
+
+    connection.connectionMethods.onConnected = () => {
+        user.id = connection.connectionId;
+        //("Name of Method to invoke on server",SocketID,Content to pass)
+        connection.invoke("ConnectedUser", connection.connectionId, JSON.stringify(user));
+
+    }
+
+    connection.connectionMethods.onDisconnected = () => {
+
+        connection.invoke("DisconnectedUser", connection.connectionId, "");
+
+    }
+
+    connection.clientMethods["pingUsers"] = (serUsers) => {
+        users = JSON.parse(serUser);
+        console.log(users);
+    };
+
+    connection.clientMethods["ReceiveUpdateAsXYColor"] = (socketId, x, y, r, g, b) => {
+        // console.log(r + " " + g + " " + b);
+        var colorToUse = "#" + r + g + b;
+        // console.log(colorToUse + "(" + x + "," + y + ")");
+        assignColorToSquare(x, y, colorToUse);
+    };
+
+    connection.start()
+
+    $(window).unload(function () {
+        connection.invoke("DisconnectedUser", connection.connectionId, "");
+    });
+}
